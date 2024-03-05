3c60a4b206cff200b84afe42d876c2c2afac1784
Gabriel Moreno <gabrielmfrontend@hotmail.com>
2024-03-04 21:25:02 -0300

    footer color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,8 +12,8 @@ const Footer = () => {
 
   return (
     <div className="mt-12 w-full max-w-3xl mx-auto border-t border-gray-500 text-center py-8 relative">
-        <p className="text-gray-500">Desenvolvido por Gabriel Moreno</p>
-        <div className="inline-flex justify-center items-center text-gray-500 gap-4 text-3xl my-4">
+        <p className="text-green-500">Desenvolvido por Gabriel Moreno</p>
+        <div className="inline-flex justify-center items-center text-green-500 gap-4 text-3xl my-4">
             <a href="https://github.com/GabrielMoreno0207" target="_blank" rel="noopener noreferrer">
                 <FaGithubSquare/>
             </a>
